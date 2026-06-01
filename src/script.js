9164e805696738c10e6d30c3e12387358a42e2f9
asai3hcy4 <asaiahcya2@gmail.com>
2026-06-01 19:15:05 +0000

    added  few scientific functions

--- src/script.js
+++ src/script.js
@@ -9,7 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const convertedValue = currentValue
         .replace("×", "*")
         .replace("÷", "/")
-        .replace("%", "*0.01");
+        .replace("%", "*0.01")
+        .replace("sin", "Math.sin")
+        .replace("cos", "Math.cos")
+        .replace("ln", "Math.log")
+        .replace("π", "Math.PI")
+        .replace("log", "Math.log10")
+        .replace("e", "Math.E")
+        .replace("√", "Math.sqrt");
 
         console.log('convertedValue:', convertedValue)
         const result = eval(convertedValue);
@@ -23,16 +30,22 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener('click', function() {
             const value = button.innerText;
 
-            if (value == "AC") {
-                currentValue ="";
-                display.value = currentValue;
-            } else if(value == "=") {
-              evaluateResult();
-            } else {
-                currentValue += value;
+            try {
+                if (value == "AC") {
+                    currentValue ="";
+                    display.value = currentValue;
+                } else if(value == "=") {
+                    evaluateResult();
+                } else {
+                    currentValue += value;
+                    display.value = currentValue;
+                }
+            } catch (error) {
+                console.error(error);
+                currentValue = "ERROR";
                 display.value = currentValue;
             }
-
+            
         })
     }
 });
